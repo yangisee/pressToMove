f07e9ab951001833046acfe457d23d4a6707658a
yangisee <yvincent74@gmail.com>
2016-01-21 19:30:05 -0500

    Initial Commit

---
+++ pressToMove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pressToMove
+//
+//  Created by YangHuairuo on 2016-01-21.
+//  Copyright © 2016 Yangisee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
