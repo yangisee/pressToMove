38434b919561ff65e167f0ff1d45582aa467739a
yangisee <yvincent74@gmail.com>
2016-01-23 20:53:10 -0500

    MoveImage
    use basic syntax to move an image by clicking buttons

--- pressToMove/ViewController.swift
+++ pressToMove/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var imge: UIImageView!
+    var location = CGPoint(x: 0, y: 0)
+    var x:CGFloat = 0.0
+    var y:CGFloat = 0.0
+    
+    @IBAction func goUp(){
+        y = y-10
+        imge.transform = CGAffineTransformMakeTranslation(x, y)
+    }
+    @IBAction func goDown(){
+        y = y+10
+        imge.transform = CGAffineTransformMakeTranslation(x, y)
+    }
+    @IBAction func goLeft(){
+        x = x-10
+        imge.transform = CGAffineTransformMakeTranslation(x, y)
+    }
+    @IBAction func goRight(){
+        x = x+10
+        imge.transform = CGAffineTransformMakeTranslation(x, y)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   
 }
 
